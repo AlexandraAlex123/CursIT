eca09eadc74f2c36ba273cc489d1c81aafb536f0
AlexandraAlex123 <alexandra_jipa@yahoo.com>
2022-08-08 17:12:22 +0300

    First commit

--- src/temaFunctii/FunctieNumereConsecutive.java
+++ src/temaFunctii/FunctieNumereConsecutive.java
@@ -4,6 +4,9 @@ public class FunctieNumereConsecutive {
     public static void main(String[] args) {
         int listInitial[] = new int[]{1, 4, 4, 5, 7, 8, 7, 2, 2, 9, 9};
         int listFinal[] = new int[listInitial.length];
+        functieNrCon(listInitial, listFinal);
+    }
+    static int[] functieNrCon(int[] listInitial, int[] listFinal){
         int j = 0;
         for (int i = 0; i < listInitial.length - 1; i++) {
             if (listInitial[i] == listInitial[i + 1]) {
@@ -12,5 +15,6 @@ public class FunctieNumereConsecutive {
                 j++;
             }
         }
+        return listFinal;
     }
 }
--- src/temaFunctii/FunctieNumerePare.java
+++ src/temaFunctii/FunctieNumerePare.java
@@ -4,6 +4,9 @@ public class FunctieNumerePare {
     public static void main(String[] args) {
         int list[] = new int[]{2, 4, 5, 8, 11, 13, 14, 17, 21, 22};
         int listPare[] = new int[list.length];
+        functieNrPare(list, listPare);
+    }
+    static int[] functieNrPare(int[] list, int[] listPare){
         int j = 0;
         for (int i = 0; i < list.length; i++) {
             if (list[i] % 2 == 0) {
@@ -13,5 +16,6 @@ public class FunctieNumerePare {
             }
 
         }
+        return listPare;
     }
 }

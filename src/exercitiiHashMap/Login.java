837fb5eb79dbc168ff0e39ac95e311b056f55d25
AlexandraAlex123 <alexandra_jipa@yahoo.com>
2022-09-29 15:38:58 +0300

    First commit

--- src/exercitiiHashMap/Login.java
+++ src/exercitiiHashMap/Login.java
@@ -1,55 +1,183 @@
 package exercitiiHashMap;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Login {
 
-    public boolean login(String user, String pwd) {
-        String url = "jdbc:mysql://localhost:3306/exercitii";
-        String username = "java";
-        String password = "password";
-
+    public String conection() {
+        String url;
+        String username;
+        String password = null;
+        try {
+            File citire = new File("C:\\Users\\Lenovo\\IdeaProjects\\untitled\\src\\exercitiiHashMap\\ConexiuneaMea");
+            Scanner myReader = new Scanner(citire);
+            while (myReader.hasNextLine()) {
+                String word = myReader.nextLine();
+                String[] s = word.split(" ");
+                if ("url".equals(s[0])) {
+                    url = s[1];
+                    return url;
+                } else if ("user".equals(s[0])) {
+                    username = s[1];
+                    return username;
+                } else if ("password".equals(s[0])) {
+                    password = s[1];
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println("Connecting database...");
+        return password;
+    }
 
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            System.out.println("Database connected!");
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("select * from users where userName = " + user + " and password = " + pwd);
-            while (rs.next()) {
-                System.out.println("User name: " + user);
-                System.out.println("Password: " + pwd);
-                System.out.println("Conecting...");
-                System.out.println("Login successful.");
+
+    public boolean login(String userName, String pwd) {
+        String url = null;
+        String username = null;
+        String password = null;
+        try {
+            File citire = new File("C:\\Users\\Lenovo\\IdeaProjects\\untitled\\src\\exercitiiHashMap\\ConexiuneaMea");
+            Scanner myReader = new Scanner(citire);
+            while (myReader.hasNextLine()) {
+                String word = myReader.nextLine();
+                String[] s = word.split(" ");
+                if ("url".equals(s[0])) {
+                    url = s[1];
+                } else if ("user".equals(s[0])) {
+                    username = s[1];
+                } else if ("password".equals(s[0])) {
+                    password = s[1];
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Connecting database...");
+        try {
+            assert url != null;
+            try (Connection connection = DriverManager.getConnection(url, username, password)) {
+                System.out.println("Database connected!");
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery("select * from login where username = " + userName + " and password = " + pwd);
+                if (rs.next()) {
+                    System.out.println("Conecting...");
+                    System.out.println("Login successful.");
+                    return false;
+                } else {
+                    return true;
+                }
             }
         } catch (SQLException e) {
             throw new IllegalStateException("Cannot connect the database!", e);
+        }
+    }
+
+    public boolean userExist(String userName) {
+        String url = null;
+        String username = null;
+        String password = null;
+        try {
+            File citire = new File("C:\\Users\\Lenovo\\IdeaProjects\\untitled\\src\\exercitiiHashMap\\ConexiuneaMea");
+            Scanner myReader = new Scanner(citire);
+            while (myReader.hasNextLine()) {
+                String word = myReader.nextLine();
+                String[] s = word.split(" ");
+                if ("url".equals(s[0])) {
+                    url = s[1];
+                } else if ("user".equals(s[0])) {
+                    username = s[1];
+                } else if ("password".equals(s[0])) {
+                    password = s[1];
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Connecting database...");
+        try {
+            assert url != null;
+            try (Connection connection = DriverManager.getConnection(url, username, password)) {
+                System.out.println("Database connected!");
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery("select username from login");
+                while (rs.next()) {
+                    if (userName.equals(rs.getString(1))) {
+                        System.out.println("This user exist.");
+                        return false;
+                    }
+                }
+                return true;
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+    }
+
+
+    public void register(String userName, String pwd) {
+        String url = null;
+        String username = null;
+        String password = null;
+        try {
+            File citire = new File("C:\\Users\\Lenovo\\IdeaProjects\\untitled\\src\\exercitiiHashMap\\ConexiuneaMea");
+            Scanner myReader = new Scanner(citire);
+            while (myReader.hasNextLine()) {
+                String word = myReader.nextLine();
+                String[] s = word.split(" ");
+                if ("url".equals(s[0])) {
+                    url = s[1];
+                } else if ("user".equals(s[0])) {
+                    username = s[1];
+                } else if ("password".equals(s[0])) {
+                    password = s[1];
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Connecting database...");
+        try {
+            assert url != null;
+            try (Connection connection = DriverManager.getConnection(url, username, password)) {
+                System.out.println("Database connected!");
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery("insert into login values('" + userName + "','" + pwd + "');");
+                if (rs.next()) {
+                    System.out.println("Your account has been created.");
+                }
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+    }
+
+
+    public boolean controlStringUser(String userName) {
+        char[] c = userName.toCharArray();
+        if (!userName.contains(" ")) {
+            return c.length < 6;
         }
         return true;
     }
 
-    public boolean register(String userName, String pwd) {
-        String url = "jdbc:mysql://localhost:3306/exercitii";
-        String username = "java";
-        String password = "password";
-
-        System.out.println("Connecting database...");
-
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            System.out.println("Database connected!");
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("select userName from users");
-            while (rs.next()) {
-                if (userName.equals(rs.getString(1))) {
-                    System.out.println("This user exist.");
-                    return false;
-                }
-            }
-            rs = statement.executeQuery("insert into user values('" + userName + "', '" + pwd + "')");
-            System.out.println("The account has been created.");
-            return true;
-        } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
+    public boolean controlStringPassword(String password) {
+        char[] c = password.toCharArray();
+        if (!password.contains(" ")) {
+            return c.length < 6;
         }
+        return true;
+    }
+
+    public boolean controlStringComanda(String comanda) {
+        char[] c = comanda.toCharArray();
+        if (!comanda.equals(" ")) {
+            return c.length != 1;
+        }
+        return true;
     }
 }
 
--- src/exercitiiHashMap/Main.java
+++ src/exercitiiHashMap/Main.java
@@ -1,14 +1,80 @@
 package exercitiiHashMap;
 
-import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner key = new Scanner(System.in);
-        Login users = new Login();
-        String user = key.nextLine();
+        Login log = new Login();
+        while (true) {
+            System.out.println("Welcome to your platform!");
+            System.out.println("Choose your action:");
+            System.out.println("1 Login");
+            System.out.println("2 Register");
+            System.out.println("3 Exit");
+            String comanda = controlComanda(log, key);
+            if ("1".equals(comanda)) {
+                System.out.print("Username: ");
+                String userName = controlUsername(log, key);
+                System.out.print("Password: ");
+                String pwd = controlPassword(log, key);
+                while (log.login(userName, pwd)) {
+                    System.out.println("Username and password dosen't match. Try again.");
+                    System.out.print("Username: ");
+                    userName = controlUsername(log, key);
+                    System.out.print("Password: ");
+                    pwd = controlPassword(log, key);
+                }
+            } else if ("2".equals(comanda)) {
+                System.out.print("Username: ");
+                String userName = controlDubluraUsername(log, key);
+                System.out.print("Password: ");
+                String pwd = controlPassword(log, key);
+                log.register(userName, pwd);
+            } else if ("3".equals(comanda)) {
+                break;
+            } else {
+                System.out.println("This command dosen't exit. Try again..");
+            }
+        }
+    }
+
+
+
+    static String controlDubluraUsername(Login log, Scanner key) {
+        String userName = controlUsername(log, key);
+        while (log.userExist(userName)) {
+            System.out.print("Username: ");
+            userName = controlUsername(log, key);
+        }
+        return userName;
+    }
+    static String controlComanda(Login log, Scanner key) {
+        String comanda = key.nextLine();
+        while (log.controlStringComanda(comanda)) {
+            System.out.println("Error. Please try again..");
+            comanda = key.nextLine();
+        }
+        return comanda;
+    }
+
+    static String controlUsername(Login log, Scanner key) {
+        String userName = key.nextLine();
+        while (log.controlStringUser(userName)) {
+            System.out.println("Error. Please try again..");
+            System.out.print("Username: ");
+            userName = key.nextLine();
+        }
+        return userName;
+    }
+
+    static String controlPassword(Login log, Scanner key) {
         String pwd = key.nextLine();
-        users.login(user,pwd);
+        while (log.controlStringPassword(pwd)) {
+            System.out.println("Error. Please try again..");
+            System.out.print("Password: ");
+            pwd = key.nextLine();
+        }
+        return pwd;
     }
 }
